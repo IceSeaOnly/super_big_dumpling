5df04287292a81070c10f387f50d4fcf3c900847
icesea <binghai@meili-inc.com>
2017-09-15 16:44:00 +0800

    Initial commit

---
+++ src/main/java/site/binghai/SuperBigDumplingApplication.java
@@ -0,0 +1,12 @@
+package site.binghai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBigDumplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperBigDumplingApplication.class, args);
+	}
+}
